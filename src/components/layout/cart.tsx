4de1377a460792923138f91292208a3a25186646
Hendra Dwi Saputra <babeguru@gmail.com>
2024-06-24 10:28:47 +0700

    Make button bigger

--- src/components/layout/cart.tsx
+++ src/components/layout/cart.tsx
@@ -4,7 +4,7 @@ import { LuShoppingCart } from "react-icons/lu";
 export function Cart() {
   return (
     <>
-      <Button isIconOnly color="danger" variant="solid"  radius="full">
+      <Button isIconOnly size="lg" color="danger" variant="solid"  radius="full">
         <LuShoppingCart className="w-5 h-5" />
       </Button>
     </>
