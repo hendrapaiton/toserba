352f871159fac0b7635d0a4e513bf58ba0b75c8a
Hendra Dwi Saputra <babeguru@gmail.com>
2024-06-24 10:27:17 +0700

    Cart button with react-icons

--- src/components/layout/cart.tsx
+++ src/components/layout/cart.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@nextui-org/react";
+import { LuShoppingCart } from "react-icons/lu";
+
 export function Cart() {
-  return <>Ini adalah Cart</>;
+  return (
+    <>
+      <Button isIconOnly color="danger" variant="solid"  radius="full">
+        <LuShoppingCart className="w-5 h-5" />
+      </Button>
+    </>
+  );
 }
