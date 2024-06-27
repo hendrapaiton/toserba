075ae79d96384605950d84253d7d9cceb98cd260
Hendra Dwi Saputra <babeguru@gmail.com>
2024-06-27 21:15:10 +0700

    Set light theme as default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
 import { Featured } from "@/components/common/featured";
 import { Hero } from "@/components/common/hero";
 import { Promo } from "@/components/common/promo";
+import { useTheme } from "next-themes";
 
 export default function Index() {
+  const { setTheme } = useTheme();
+  useEffect(() => {
+    setTheme("light");
+  }, []);
+
   return (
     <>
       <Hero />
